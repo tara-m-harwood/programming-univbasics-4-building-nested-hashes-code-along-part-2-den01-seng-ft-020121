da14fd3c048901733a69736467a9f010bf63fbe7
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-09 20:09:08 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -27,8 +27,6 @@ def update_status
    epic_tragedy
  end
 
-
-
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
   epic_tragedy
 
